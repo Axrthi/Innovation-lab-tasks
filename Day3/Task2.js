0fac2d0482a3f639b478a2a93727e46407bae16c
Axrthi <an2006324@gmail.com>
2025-07-27 20:47:33 +0530

    Task2.js

---
+++ Day3/Task2.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+
+const add = function(a, b) {
+    return a + b;
+};
+
+console.log(add(2, 3));
+
+const add = (a, b) => a + b;
+
+console.log(add(2, 3)); 
